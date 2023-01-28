3c25f6b31971c61b093e432e337ebf14e90b79e4
Calvin-Spens <calvinsjohnson.com@gmail.com>
2023-01-27 17:42:25 -0800

    changed space

--- python_scripting/kata_testing/open_or_senior.py
+++ python_scripting/kata_testing/open_or_senior.py
@@ -30,4 +30,4 @@ def open_or_senior(data):
 
     return ["Senior" if age >= 55 and handicap >= 8 else "Open" for (age, handicap) in data]
 
- print(open_or_senior([(45, 12),(55,21),(19, -2),(104, 20)])) 
+print(open_or_senior([(45, 12),(55,21),(19, -2),(104, 20)]))
