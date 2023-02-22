a8d8bc377ceb6cb0822db54a329e5d559c4a2547
Calvin-Spens <calvinsjohnson.com@gmail.com>
2023-02-22 13:47:09 -0800

    Finished problem 1 in problem set 3

---
+++ python_scripting/cs50_problems/problem_set_3.py
@@ -0,0 +1,24 @@
+def main():
+    fuel()
+
+def fuel():
+    tank = float(tank_status()) * 100
+    if tank <= 1:
+        print('E')
+    elif tank >= 99:
+        print('F')
+    else:
+        print(str(int(tank)) + "%")
+
+def tank_status():
+     while True:
+        fract = input("Fraction: ").split("/")
+        try:
+            gas = int(fract[0]) / int(fract[1])
+        except: 
+            print("That's not a proper fraction")
+            continue
+        if gas <= 1:
+            return f'{gas:.2f}'
+        print("Can't overfill your gas tank")
+main()
