a989491a5fcb56a28f14c1fc190fd97a0c0f1cc5
Calvin-Spens <calvinsjohnson.com@gmail.com>
2023-10-13 12:14:06 -0700

    working on problem 2 from set 4

--- python_scripting/cs50_problems/problem_set_4.py
+++ python_scripting/cs50_problems/problem_set_4.py
@@ -1,19 +1,37 @@
 import emoji
+from pyfiglet import Figlet
+import sys
 
-def main():
-    print("Which Program do you want to run?")
-    print("1: emojize")
-    program = input(">> ")
-    match program:
-        case '1':
-            emojize()
-        case _:
-            print("Not a valid option")    
-    return            
+# def main():
+#     print("Which Program do you want to run?")
+#     print("1: emojize")
+#     print("q: exit")
+#     which_func = input(">> ")
+#     match which_func:
+#         case '1':
+#             emojize()
+#         case '2': 
+#             FIGlet()
+#         case 'q' | 'Q':
+#             return
+#         case _:
+#             print("Not a valid option")    
+#     main()            
 
-def emojize():
-    e = input('What emoji do you want to see? ')
-    print(emoji.emojize(e))
+# def emojize():
+#     e = input('What emoji do you want to see? ')
+#     print(emoji.emojize(e))
 
-if __name__ == "__main__":
-    main()
+def FIGlet():
+    figlet = Figlet()
+
+    if len(sys.argv) == 3:
+        if sys.argv != ''
+    else:    
+        sys.exit("Invalid usage")
+    phrase = input("Input: ")
+    print(figlet.renderText(phrase))
+
+FIGlet()
+# if __name__ == "__main__":
+#     main()
