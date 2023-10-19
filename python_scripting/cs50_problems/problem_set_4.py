153252036ad1969fdb75a511c86135a602321992
Calvin-Spens <calvinsjohnson.com@gmail.com>
2023-10-18 17:35:21 -0700

    better version for problem 2 set 4

--- python_scripting/cs50_problems/problem_set_4.py
+++ python_scripting/cs50_problems/problem_set_4.py
@@ -28,14 +28,15 @@ def FIGlet():
 
     if len(sys.argv) == 1:
         figlet = Figlet(font=random.choice(Figlet().getFonts()))
-    elif len(sys.argv) == 3:
-        if sys.argv[1] == "-f" or sys.argv[1] == "--font":
-            if sys.argv[2] in Figlet().getFonts():
-                figlet = Figlet(font=sys.argv[2]) 
-            # else:
-            #     sys.exit("not correct font")
-    else:    
-        sys.exit("Invalid usage")
+    else:
+        match sys.argv[1]:
+            case "-f" | "--font":
+                if sys.argv[2] in Figlet().getFonts():
+                    figlet = Figlet(font=sys.argv[2]) 
+                else:
+                    sys.exit("Invalid usage")
+            case _:
+                sys.exit("Invalid usage")
 
 
     phrase = input("Input: ")
